da8d3bdef92ab9126a48b998ab1681cd2b9d7a11
Constantino Schillebeeckx <titous@gmail.com>
2017-08-23 09:45:36 -0500

    update

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1,4 +1,4 @@
-/* nvd3 version 1.8.5-dev (https://github.com/novus/nvd3) 2017-08-22 */
+/* nvd3 version 1.8.5-dev (https://github.com/novus/nvd3) 2017-08-23 */
 (function(){
 
 // set up main nv object
